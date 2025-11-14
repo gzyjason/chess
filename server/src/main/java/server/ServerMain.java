df546bf24614f2e1234e41effbffc5913b968b1c
Melvin Whitaker <awsome7mailman@gmail.com>
2025-11-14 14:33:31 -0700

    Renamed Main classes to 'ClientMain' and 'ServerMain'

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,10 +1,10 @@
-package client;
+package server;
 
 import chess.*;
 
-public class Main {
+public class ServerMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Server: " + piece);
     }
 }

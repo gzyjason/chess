4659d90fadc5373ed58261904aa8541b6f2ef560
gzyjason <gzyjason@gmail.com>
2026-01-20 15:52:55 -0700

    Wrote logic to handle special movement of bishop, rook, queen, which allows them to travel to the edges.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,6 +14,8 @@ public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
+    private int[] temp = new int[2];
+    private ArrayList<ChessPosition> result = new ArrayList<>();
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
@@ -56,8 +59,25 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
         if (piece.getPieceType() == PieceType.BISHOP) {
-            return List.of(new ChessMove());
+
         }
         return List.of();
+    }
+
+    public void BoardEdge(ChessBoard board, ChessPosition myPosition, int[] row, int[]col){
+        for(int i = 0; i < row.length; i++){
+            temp[1] = myPosition.getColumn() + col[i];
+            temp[0] = myPosition.getRow() + row[i];
+            while (temp[0] >= 1 && temp[0] <= 8 && temp[1] >= 1 && temp[1] <=8){
+                ChessPosition tempPosition = new ChessPosition(temp[0], temp[1]);
+                if(board.getPiece(tempPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
+                     result.add(tempPosition);
+                     break;
+                }
+                result.add(tempPosition);
+                temp[0] = temp[0] + row[i];
+                temp[1] = temp[1] + col[i];
+            }
+        }
     }
 }
---
+++ shared/src/main/java/chess/MoveCalculations/QueenMoves.java
@@ -0,0 +1,4 @@
+package chess.MoveCalculations;
+
+public class QueenMoves {
+}

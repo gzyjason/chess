5370361e46f48b8112066cd95950d7c39e647513
Jason Gao <gzyjason@gmail.com>
2026-03-09 19:06:45 -0600

    Finished writing all methods in SqlAuthDAO and implemented the unit tests for them

--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -1,7 +1,6 @@
 package dataaccess;
 
 import model.AuthData;
-
 import java.sql.SQLException;
 
 public class SqlAuthDAO implements AuthDAO{
@@ -22,26 +21,61 @@ public class SqlAuthDAO implements AuthDAO{
     }
     @Override
     public void createToken(AuthData auth) throws DataAccessException {
+        String statement = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        try (var openConnection = DatabaseManager.getConnection( );
+             var getReady = openConnection.prepareStatement(statement)){
+            getReady.setString(1, auth.authToken());
+            getReady.setString(2, auth.username());
 
+            getReady.executeUpdate();
+        } catch (SQLException exception) {
+            throw new DataAccessException(String.format("Error creating token: %s", exception.getMessage()), exception);
+        }
     }
 
     @Override
     public AuthData getToken(String authToken) throws DataAccessException {
+        String statement = "SELECT authToken, username FROM auth WHERE authToken =?";
+        try (var openConnection = DatabaseManager.getConnection();
+             var getReady = openConnection.prepareStatement(statement)) {
+
+            getReady.setString(1, authToken);
+
+            try (var results = getReady.executeQuery()) {
+                if (results.next()) {
+                    String returnedAuthToken = results.getString("authToken");
+                    String returnedUsername = results.getString("username");
+
+                    return new AuthData(returnedAuthToken, returnedUsername);
+                }
+            }
+
+        } catch (SQLException exception) {
+            throw new DataAccessException(String.format("Error retrieving token: %s", exception.getMessage()), exception);
+        }
         return null;
     }
+
     @Override
     public void deleteToken(String authToken) throws DataAccessException{
-
+        String statement ="DELETE FROM auth WHERE authToken = ?";
+        try (var openConnection = DatabaseManager.getConnection();
+             var getReady = openConnection.prepareStatement(statement)) {
+            getReady.setString(1, authToken);
+            getReady.executeUpdate();
+        } catch (SQLException exception){
+            throw new DataAccessException(String.format("Error deleting token: %s", exception.getMessage()), exception);
+        }
     }
 
     @Override
     public void clear() throws DataAccessException {
-        String statement = "TRUNCATE TABLE user";
+        String statement = "TRUNCATE TABLE auth";
         try (var openConnection = DatabaseManager.getConnection();
              var getReady = openConnection.prepareStatement(statement)) {
             getReady.executeUpdate();
         } catch (SQLException exception){
-            throw new DataAccessException(String.format("Error clearing player: %s", exception.getMessage()), exception);
+            throw new DataAccessException(String.format("Error clearing token: %s", exception.getMessage()), exception);
         }
 
     }
---
+++ server/src/test/java/dataaccess/SqlAuthDAOTests.java
@@ -0,0 +1,81 @@
+package dataaccess;
+import model.AuthData;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SqlAuthDAOTests {
+    private SqlAuthDAO authDAO;
+    @BeforeEach
+    public void setUp() throws DataAccessException {
+        authDAO = new SqlAuthDAO();
+        authDAO.clear();
+    }
+
+    @Test
+    public void createTokenPositive() throws DataAccessException{
+        AuthData testToken = new AuthData("randomToken", "Tom");
+        authDAO.createToken(testToken);
+        AuthData returnedToken = authDAO.getToken("randomToken");
+
+        assertNotNull(returnedToken, "The returned token shouldn't be null");
+        assertEquals(testToken, returnedToken, "The returned token should match the created token");
+    }
+
+    @Test
+    public void createTokenNegative() throws DataAccessException{
+        AuthData testToken = new AuthData("randomToken", "Tom");
+        authDAO.createToken(testToken);
+        AuthData testTokenAgain = new AuthData("randomToken", "Jack");
+        assertThrows(DataAccessException.class, () -> {
+            authDAO.createToken(testTokenAgain);
+        }, "Shouldn't be able to create duplicate tokens");
+    }
+
+    @Test
+    public void getTokenPositive() throws DataAccessException {
+        AuthData testToken = new AuthData("randomToken", "Tom");
+        authDAO.createToken(testToken);
+        AuthData returnedToken = authDAO.getToken("randomToken");
+
+        assertNotNull(returnedToken, "The returned token shouldn't be null");
+        assertEquals(testToken, returnedToken, "The returned token should match the created token");
+    }
+
+    @Test
+    public void getTokenNegative() throws DataAccessException{
+        AuthData testToken = new AuthData("randomToken", "Tom");
+        authDAO.createToken(testToken);
+        AuthData returnedToken = authDAO.getToken("fakeToken");
+        assertNull(returnedToken, "Should not be able to retrieve nonexisting token");
+    }
+
+    @Test
+    public void deleteTokenPositive() throws DataAccessException{
+        AuthData testToken = new AuthData("randomToken", "Tom");
+        authDAO.createToken(testToken);
+        authDAO.deleteToken("randomToken");
+        AuthData returnedToken = authDAO.getToken("randomToken");
+        assertNull(returnedToken, "Getting deleted token should result in null");
+    }
+
+    @Test
+    public void deleteToken_Negative_NonExistent() throws DataAccessException {
+        AuthData testToken = new AuthData("randomToken", "Tom");
+        authDAO.createToken(testToken);
+
+        assertDoesNotThrow(() -> {
+            authDAO.deleteToken("fakeToken");
+        }, "Deleting a non-existent token should not throw an exception");
+
+        assertNotNull(authDAO.getToken("randomToken"), "The existing token should still exist");
+    }
+
+    @Test
+    public void clearPositive() throws DataAccessException{
+        AuthData testToken = new AuthData("randomToken", "Tom");
+        authDAO.createToken(testToken);
+        authDAO.clear();
+        AuthData cleared = authDAO.getToken("randomToken");
+        assertNull(cleared, "The token shouldn't exist after clear is called");
+    }
+}
